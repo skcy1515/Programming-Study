d5c9b60bacf3016f5902e42fc805ee945d9e0ad6
skcy1515 <skcy151515@gmail.com>
2024-05-24 23:28:15 +0900

    유저 조회 API 추가

--- Spring/All-In-One/library-app/src/main/java/com/group/libraryapp/Controller/user/UserController.java
+++ Spring/All-In-One/library-app/src/main/java/com/group/libraryapp/Controller/user/UserController.java
@@ -1,7 +1,9 @@
 package com.group.libraryapp.Controller.user;
 
 import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.dto.user.userCreateRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +19,14 @@ public class UserController {
     @PostMapping("/user")
     public void saveUser(@RequestBody userCreateRequest request) {
         users.add(new User(request.getName(), request.getAge()));
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        List<UserResponse> responses = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            responses.add(new UserResponse(i + 1, users.get(i)));
+        }
+        return responses;
     }
 }
